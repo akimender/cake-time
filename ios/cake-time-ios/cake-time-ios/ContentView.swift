a831724ec4de7a69917b7716c45888b5fab50b16
Andrew Kim <andrew_kim3@brown.edu>
2026-02-25 22:26:06 +0100

    add ios boilerplate and add env for backend url in web

---
+++ ios/cake-time-ios/cake-time-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cake-time-ios
+//
+//  Created by Andrew Kim on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/cake-time-ios/cake-time-ios/cake_time_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cake_time_iosApp.swift
+//  cake-time-ios
+//
+//  Created by Andrew Kim on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct cake_time_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
