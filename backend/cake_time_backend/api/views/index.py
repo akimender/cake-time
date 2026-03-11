bfa8734f2645544f024be61e48b49707391c5eb1
Andrew Kim <andrew_kim3@brown.edu>
2026-03-11 11:34:35 +0100

    test api endpoint allows any

--- backend/cake_time_backend/api/views/index.py
+++ backend/cake_time_backend/api/views/index.py
@@ -1,9 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
 
 @api_view(['GET'])
-def index():
+@permission_classes([AllowAny])
+def index(request):
     """Return OK response for calling GET to this endpoint -- tests the prod server"""
     return Response(status=status.HTTP_200_OK)
